29bf262b798787cf48641b188b8bb8570cf585df
VISHALVELLINGIRINATHAN <34641037+VISHALVELLINGIRINATHAN@users.noreply.github.com>
2017-12-19 12:38:54 +0530

    Create vowel or not.c

---
+++ code kata/Beginner level/vowel or not.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+ main() 
+ {
+ 	char x;
+	scanf("%c",&x);
+	if((x=='a'||x=='A')||(x=='e'||x=='E')||(x=='i'||x=='I')||(x=='o'||x=='O')||(x=='u'||x=='U'))
+	
+	{
+		printf("vowel");
+	}
+	else 
+	{
+		printf("consonant");
+	}
+	
+}
