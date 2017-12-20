9c43446353334bf0f3124e1968649d794ad4171c
VISHALVELLINGIRINATHAN <34641037+VISHALVELLINGIRINATHAN@users.noreply.github.com>
2017-12-20 15:16:57 +0530

    Create swap odd and even characters.c

---
+++ Assignments/swap odd and even characters.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[50];
+	int i,j,len;
+	gets(a);
+      printf("%s\n",a);	
+   len=strlen(a);
+   printf("%d\n",len);
+      len=len%2==0?len:len+1;
+      for(i=0,j=1;i<=len,j<=len+1;i+=2,j+=2)
+      printf("%c%c",a[j],a[i]);
+      return 0;
+}
