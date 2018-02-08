ae43e86bd48ae68a7f19c97a015d16bb49fa84f6
VISHALVELLINGIRINATHAN <34641037+VISHALVELLINGIRINATHAN@users.noreply.github.com>
2018-02-08 07:33:25 -0800

    Update armstrongdisplay.c

--- code kata/Beginner level/armstrongdisplay.c
+++ code kata/Beginner level/armstrongdisplay.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main()
 {
-  int n1, n2, i, temp, num, rem;
+  int n1, n2, i, tempnum, numer, rem;
   scanf("%d %d", &n1, &n2);
   for(i=n1+1; i<n2; ++i)
   {
       temp=i;
       num=0;
-      while(temp!=0)
+      while(tempnum!=0)
       {
           rem=(temp%10);
-          num+=rem*rem*rem;
+          numerr+=rem*rem*rem;
           temp/=10;
       }
-      if(i==num)
+      if(i==numer)
       {
           printf("%d ",i);
       }
