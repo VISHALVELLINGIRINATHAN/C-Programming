6285321b480e649cf1619a7738bb56cf73185471
VISHAL.V <34641037+VISHALVELLINGIRINATHAN@users.noreply.github.com>
2018-03-07 08:38:37 -0800

    Create repeat.c

---
+++ codekata/player/repeat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ int main(void) 
+{
+    char st[50],ch;
+	int i,j,count=1,max=1;
+	scanf("%s",st);
+	for(i=0;st[i]!='\0';i++)
+	{
+		for(j=i+1;st[j]!='\0';j++)
+		if(st[i]==st[j])
+		{
+			count=count+1;
+			if(count>max)
+			{
+				max=count;
+				ch=st[i];
+			}
+		}
+ 
+	}
+	printf("%c",ch);
+	return 0;
+}
