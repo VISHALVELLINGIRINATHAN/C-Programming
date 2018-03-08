1179980bf8408bf88712a6c2494aaae0acbf16cf
VISHAL.V <34641037+VISHALVELLINGIRINATHAN@users.noreply.github.com>
2018-03-08 13:54:39 +0530

    Update med.c

--- code kata/Beginner level/med.c
+++ code kata/Beginner level/med.c
@@ -1,16 +1,19 @@
-#include<stdio.h> 
-void main()
+#include <stdio.h>
+int main(void) {
+int i,j,a[50],temp,num;
+scanf("%d",&num);
+for(i=0;i<num-1;i++)
 {
-int a[10],i,n;
-scanf("%d",&n);
-for(i=1;i<=n;i++)
+for(j=i;j<num;j++)
 {
-printf("\n %d",i);
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
 }
-int l=a[0],h=n,mid;
-if(l<h)
-{
-	mid=(l+h)/2;
-	printf("\n%d",mid);
+printf("medium element is %d",a[num/2]);
+		
 }
+return 0;
 }
