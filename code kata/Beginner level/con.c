0c1e1ac3cfa4bcf207ca837d332ed4115e5afdb3
VISHAL.V <34641037+VISHALVELLINGIRINATHAN@users.noreply.github.com>
2018-05-18 08:04:39 -0700

    Create con.c

---
+++ code kata/Beginner level/con.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) 
+
+{
+
+	int num,count=0;
+	scanf("%d",&num);
+
+	while(num!=0)
+
+	{
+
+		num=num/10;
+
+		count++;
+
+	}
+
+	printf("\n%d",count);
+
+	return 0;
+}
